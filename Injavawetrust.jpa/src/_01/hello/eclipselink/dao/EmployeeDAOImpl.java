6fa65078bc92e3fd1cfbcad60dc474a9ab9c82eb
leventerguder <erguder.levent@gmail.com>
2016-10-07 20:37:45 +0300

    example1

--- Injavawetrust.jpa/src/_01/hello/eclipselink/dao/EmployeeDAOImpl.java
+++ Injavawetrust.jpa/src/_01/hello/eclipselink/dao/EmployeeDAOImpl.java
@@ -1,11 +1,7 @@
 package _01.hello.eclipselink.dao;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import _01.hello.eclipselink.model.Employee;
@@ -13,22 +9,15 @@ import _01.hello.eclipselink.model.Employee;
 public class EmployeeDAOImpl implements EmployeeDAO {
 
 	private EntityManager entityManager;
-	private EntityManagerFactory entityManagerFactory;
-	private EntityTransaction entityTransaction;
 
-	public EmployeeDAOImpl() {
-		entityManagerFactory = Persistence.createEntityManagerFactory("EmployeePersistenceUnit");
-		entityManager = entityManagerFactory.createEntityManager();
-		entityTransaction = entityManager.getTransaction();
-
+	public EmployeeDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
 	}
 
 	@Override
 	public Employee insertEmployee(int id, String name, String surname, int salary) {
 		Employee employee = new Employee(id, name, surname, salary);
-		entityTransaction.begin();
 		entityManager.persist(employee);
-		entityTransaction.commit();
 		return employee;
 	}
 
@@ -49,9 +38,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	public void removeEmployee(int id) {
 		Employee employee = findEmployee(id);
 		if (employee != null) {
-			entityTransaction.begin();
-			entityManager.remove(employee);
-			entityTransaction.commit();
+			entityManager.remove(employee);			
 		}
 	}
 
@@ -59,9 +46,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	public Employee raiseEmployeeSalary(int id, int raise) {
 		Employee employee = findEmployee(id);
 		if (employee != null) {
-			entityTransaction.begin();
 			employee.setSalary(employee.getSalary() + raise);
-			entityTransaction.commit();
 		}
 		return employee;
 
--- Injavawetrust.jpa/src/_02/field/access/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package _02.field.access.service;
-
-import _02.field.access.model.Employee2;
-
-public interface EmployeeService {
-
-	public Employee2 createEmployee(int id, String name, String surname, int salary);
-
-}
